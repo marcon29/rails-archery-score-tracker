78921af6f37383e6aaec92b0df38abe3a38098f5
marcon29 <online@markstabler.com>
2020-10-24 09:33:38 -0600

    added some outline details to End

--- app/models/end.rb
+++ app/models/end.rb
@@ -14,15 +14,53 @@ class End < ApplicationRecord
         #     :set_score ( 0 - 2 )
         # format: :number (number), :set_score (number)
 
-        # set_score validation rules
-            # must be able to be blank on instantiation
-            # if end.round.score_method == "Set", can't be blank (only when updating)
-            # if end.round.score_method == "Points", must be blank (just delete any entry)
+    validates :number, 
+        numericality: {
+            only_integer: true, 
+            greater_than: 0, 
+            # real code
+            # less_than_or_equal_to: ends_in_set.count
+            # use until assoc set
+            less_than_or_equal_to: 6
+        }
+    validates :set_score, 
+        numericality: {
+            only_integer: true, 
+            greater_thanor_equal_to: 0, 
+            less_than_or_equal_to: 2, 
+            allow_nil: true, 
+            message: "You must enter 0, 1, or 2."
+        }
+    before_validation :assign_number
+    
+    # #################################################
+    # need associations set up before I can really do anything
+    # #################################################
+
+    # set_score validation rules
+        # must be able to be blank on instantiation
+        # if end.round.score_method == "Set", can't be blank (only when updating)
+        # if end.round.score_method == "Points", must be blank (just delete any entry)
 
 
     # need helpers (callbacks & validations)
-    # need to auto create number ( sequential by rset )
-        # SetEndFormat.name will be input by controller, not via assoc.
+    def assign_number
+        self.number = ends_in_set.count + 1
+    end
+
+
+    def ends_in_set
+        # real code
+        # self.rset.ends
+
+        # use until assoc set
+        ["end1", "end2", "end3", "end4", "end5"]
+    end
+
+    
+        
+        # need to auto create number ( sequential by rset )
+            # endd.name will be input by controller, not via assoc.
 
 
     # need helpers
