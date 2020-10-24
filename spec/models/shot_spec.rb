4d5fffcd943f66e8926f2085210d11d68540a037
marcon29 <online@markstabler.com>
2020-10-24 01:46:07 -0600

    updated Shot specs template (not tests themselves yet)

--- spec/models/shot_spec.rb
+++ spec/models/shot_spec.rb
@@ -1,28 +1,29 @@
 require 'rails_helper'
 
 RSpec.describe Shot, type: :model do
-    # add attrs (must be in hash) for all regular valid instances of Shot
-        # one with all attrs defined and one with only required attrs defined
-        # doing this separately makes writing the expect statments easier
+    # ###################################################################
+    # define main test object
+    # ###################################################################
+    # needs to be different from valid object in RailsHelper to avoid duplicte failures
     let(:valid_all) {
-        {date: "2020-09-01", end_num: 5, shot_num: 5, score_entry: "5",  set_score: 2}
+        {date: "2010-09-01", end_num: 5, shot_num: 5, score_entry: "5",  set_score: 2}
     }
 
-    let(:multi_valid_all) {
-        multi_shot_11: {date: "2020-09-01", end_num: 1, shot_num: 1, score_entry: "X",  set_score: 2}
-        multi_shot_12: {date: "2020-09-01", end_num: 1, shot_num: 2, score_entry: "10", set_score: 2}
-        multi_shot_13: {date: "2020-09-01", end_num: 1, shot_num: 3, score_entry: "M",  set_score: 2}
-        multi_shot_21: {date: "2020-09-01", end_num: 2, shot_num: 1, score_entry: "9",  set_score: ""}
-        multi_shot_22: {date: "2020-09-01", end_num: 2, shot_num: 2, score_entry: "8",  set_score: ""}
-        multi_shot_23: {date: "2020-09-01", end_num: 2, shot_num: 3, score_entry: "7",  set_score: ""}
-    }
-    
-    # create the main valid test instances, using the valid_all attrs (not persisted until called)
-        # only add multiple instantiations if need multiple instances at the same time for testing
-        # if just need to adjust attr values, use the attr sets below
-    
     let(:test_shot) {
         Shot.create(valid_all)
+    }
+    
+    # ###################################################################
+    # define any additional objects to test for this model 
+    # ###################################################################
+    # only add multiple instantiations if need simultaneous instances for testing
+    let(:multi_valid_all) {
+        multi_shot_11: {date: "2010-09-01", end_num: 1, shot_num: 1, score_entry: "X",  set_score: 2}
+        multi_shot_12: {date: "2010-09-01", end_num: 1, shot_num: 2, score_entry: "10", set_score: 2}
+        multi_shot_13: {date: "2010-09-01", end_num: 1, shot_num: 3, score_entry: "M",  set_score: 2}
+        multi_shot_21: {date: "2010-09-01", end_num: 2, shot_num: 1, score_entry: "9",  set_score: ""}
+        multi_shot_22: {date: "2010-09-01", end_num: 2, shot_num: 2, score_entry: "8",  set_score: ""}
+        multi_shot_23: {date: "2010-09-01", end_num: 2, shot_num: 3, score_entry: "7",  set_score: ""}
     }
 
     let(:test_multi) {
@@ -30,81 +31,24 @@ RSpec.describe Shot, type: :model do
             let(:shot) { Shot.create(attrs) }
         end
     }
+
+    # ###################################################################
+    # define standard create/update variations
+    # ###################################################################
     
-    # add all instances of AssocModel needed for testing associations (not persisted until called)
-        # should be valid with all attrs
-    let(:assoc_archer) {
-        Archer.create(
-            username: "testuser", 
-            email: "testuser@example.com", 
-            password: "test", 
-            first_name: "Test", 
-            last_name: "User", 
-            birthdate: "1980-07-01", 
-            gender: "Male", 
-            home_city: "Denver", 
-            home_state: "CO", 
-            home_country: "USA", 
-            default_age_class: "Senior"
-        )
-    }
-
-    let(:assoc_score_session) {
-        ScoreSession.create(
-            name: "2020 World Cup", 
-            score_session_type: "Tournament", 
-            city: "Oxford", 
-            state: "OH", 
-            country: "USA", 
-            start_date: "2020-09-01", 
-            end_date: "2020-09-05", 
-            rank: "1st", 
-            active: true
-        )
-    }
-
-    let(:assoc_round) {
-        Round.create(name: "1440 Round", discipline: "Outdoor", round_type: "Qualifying", num_roundsets: 4, user_edit: false)
-    }
-
-    let(:assoc_set) {
-        Rset.create(name: "1440 Round - Set/Distance1", ends: 6, shots_per_end: 6, score_method: "Points")
-    }
-
-    let(:assoc_category) {
-        ArcherCategory.create(
-        cat_code: "WA-RM", 
-        gov_body: "World Archery", 
-        cat_division: "Recurve", 
-        cat_age_class: "Senior", 
-        min_age: 21, 
-        max_age: 49, 
-        open_to_younger: true, 
-        open_to_older: true, 
-        cat_gender: "Male"
-        )
-    }
-
-    let(:assoc_target) {
-        Target.create(size: "122cm", score_areas: 10, rings: 10, x_ring: true, max_score: 10, spots: 1, user_edit: false)
-    }
-
-    let(:assoc_dist_targ) {
-        DistanceTargetCategory.create(distance: "90m", target_id: 1, archer_category_id: 1, rset_id: 1)
-    }
-    
-    # take valid_all and remove any non-required atts and auto-assign (not auto_format) attrs, all should be formatted correctly already
+    # take test_all and remove any non-required attrs and auto-assign (not auto_format) attrs, all should be formatted correctly
     let(:valid_req) {
         {date: "2020-09-01", end_num: 5, shot_num: 5, score_entry: "5"}
     }
 
-    # exact duplicate of valid_all - use as whole for testing unique values
-    # use for testing specific atttrs (bad inclusion, bad format, helpers, etc.)
+    # exact duplicate of test_all
+        # use as whole for testing unique values
+        # use for testing specific atttrs (bad inclusion, bad format, helpers, etc.) - change in test itself
     let(:duplicate) {
         {date: "2020-09-01", end_num: 5, shot_num: 5, score_entry: "5",  set_score: 2}
     }
 
-    # start w/ valid_all, change all values, make any auto-assign blank (don't delete)
+    # start w/ test_all, change all values, make any auto-assign blank (don't delete), delete any attrs with DB defaults
     let(:update) {
         {date: "2020-09-05", end_num: 6, shot_num: 6, score_entry: "6",  set_score: ""}
     }
@@ -114,25 +58,39 @@ RSpec.describe Shot, type: :model do
         {date: "", end_num: "", shot_num: "", score_entry: "",  set_score: ""}
     }
   
-    # add the following default error messages for different validation failures (delete any unnecessary for model)
-    let(:default_missing_message) {"can't be blank"}
-    let(:default_duplicate_message) {"has already been taken"}
-    let(:default_inclusion_message) {"is not included in the list"}
-    let(:default_number_message) {"is not a number"}
-    let(:default_format_message) {"is invalid"}
-    
+    # ###################################################################
+    # define test results for auto-assign attrs
+    # ###################################################################
+    # let(:assigned_attr) {}
+    # let(:default_attr) {}
+  
+    # ###################################################################
+    # define custom error messages
+    # ###################################################################
     let(:missing_score_entry_message) {"You must provide a rset score for shot #{shot.shot_num}."}
     let(:missing_set_score_message) {"You must enter a set score for the end."}
     # let(:inclusion_date_message) {"Date must be between #{assoc_score_session.start_date} and #{assoc_score_session.end_date}."}
+    
+    # let(:missing_attr_message) {}
+    # let(:duplicate_attr_message) {}
+    # let(:inclusion_attr_message) {}
+    # let(:number_attr_message) {}
+    # let(:format_attr_message) {}
+
+    # ###################################################################
+    # define tests
+    # ###################################################################
+    
 
     # object creation and validation tests #######################################
-    describe "model creates and updates only valid instances" do
-        describe "valid with all required and unrequired input data" do
-            it "instance is valid with all attributes" do
+    describe "model creates and updates only valid instances - " do
+        describe "valid when " do
+            it "given all required and unrequired attributes" do
                 expect(Shot.all.count).to eq(0)
 
                 expect(test_shot).to be_valid
                 expect(Shot.all.count).to eq(1)
+                
                 expect(test_shot.date).to eq(valid_all[:date])
                 expect(test_shot.end_num).to eq(valid_all[:end_num])
                 expect(test_shot.shot_num).to eq(valid_all[:shot_num])
@@ -140,36 +98,42 @@ RSpec.describe Shot, type: :model do
                 expect(test_shot.set_score).to eq(valid_all[:set_score])
             end
             
-            it "instance is valid with only required attributes" do
+            it "given only required attributes" do
                 expect(Shot.all.count).to eq(0)
                 shot = Shot.create(valid_req)
 
                 expect(shot).to be_valid
                 expect(Shot.all.count).to eq(1)
+
+                # req input tests (should have value in test_req)
                 expect(shot.date).to eq(valid_req[:date])
                 expect(shot.end_num).to eq(valid_req[:end_num])
                 expect(shot.shot_num).to eq(valid_req[:shot_num])
                 expect(shot.score_entry).to eq(valid_req[:score_entry])
                 expect(shot.set_score).to eq(valid_req[:set_score])
-            end
-
-            it "instance creates any auto-generated data for attrs" do
-                pending "see if use this for auto creation of set_score"
-            end
-
-            it "instance is valid when updating all attrs" do
-                test_shot.update(update)
                 
+                # not req input tests (<list attrs> auto-asigned from missing)
+                # add expect for all non-req attrs
+            end
+
+            it "updating all attributes" do
+                test_shot.update(update)
+
                 expect(test_shot).to be_valid
+
+                # req input tests (should have value in update)
                 expect(test_shot.date).to eq(update[:date])
                 expect(test_shot.end_num).to eq(update[:end_num])
                 expect(test_shot.shot_num).to eq(update[:shot_num])
                 expect(test_shot.score_entry).to eq(update[:score_entry])
                 expect(test_shot.set_score).to eq(update[:set_score])
+                
+                # not req input tests (<list attrs> auto-asigned from blank)
+                # add expect for all auto-assigned attrs
             end
         end
 
-        describe "invalid and has correct error message if" do
+        describe "invalid and has correct error message when" do
             it "missing required attributes" do
                 shot = Shot.create(blank)
 
@@ -184,14 +148,9 @@ RSpec.describe Shot, type: :model do
                 # expect(shot.errors.messages[:date]).to include(inclusion_date_message)
             end
 
-            it "missing set_score during a Set with 'Set' score method" do
-                assoc_set.update(score_method: "Set")
-                shot = Shot.create(blank)
-
-                expect(shot).to be_invalid
-                expect(Shot.all.count).to eq(0)
-                expect(shot.errors.messages[:set_score]).to include(missing_set_score_message)
-            end
+    #########################
+            # add unique attr tests back in?????
+    #########################
             
             it "end_num is outside allowable inputs" do
                 bad_scenarios = ["0", "-1", "7", "bad"]
@@ -256,6 +215,8 @@ RSpec.describe Shot, type: :model do
     # association tests ########################################################
     describe "instances are properly associated to other models" do
         before(:each) do
+            # load test object
+            
             # load all AssocModels that must be in DB for tests to work
         end
 
