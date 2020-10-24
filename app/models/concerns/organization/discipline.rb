ee3c9d26ae1f8a385fc581743969fe0dffb6b2cc
marcon29 <online@markstabler.com>
2020-10-24 16:55:08 -0600

    generated and planned Discipline model

--- app/models/concerns/organization/discipline.rb
+++ app/models/concerns/organization/discipline.rb
@@ -1,16 +1,13 @@
 module Organization
-    class GovBody < ApplicationRecord
+    class Discipline < ApplicationRecord
         # has_many :archer_categories
-        # has_many :disciplines, :divisions, :age_classes, :genders, through :archer_categories
+        # has_many :gov_bodies, through :archer_categories
 
-        # all attrs - :name, :org_type, :geo_area
+        # all attrs - :name
         
         # validates :name, 
         #     presence: { message: "You must enter a name." }, 
         #     uniqueness: { case_sensitive: false, message: "That name is already taken." }
-        # validates :org_type, 
-        #     presence: { message: "You must choose an organization type." }, 
-        #     inclusion: { in: ORG_TYPES }
         # before_validation :format_name
 
         # helpers (callbacks & validations)
@@ -18,13 +15,7 @@ module Organization
         #     self.name = self.name.titlecase
         # end
 
-        # def format_geo_area
-        #     self.geo_area = self.geo_area.titlecase
-        # end
-
         # helpers (data)
-
+        
     end
 end
-
-
