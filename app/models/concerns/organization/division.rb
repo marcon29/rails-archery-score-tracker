f4ccd921c23b3ea2ee8207342175160c4f802572
marcon29 <online@markstabler.com>
2020-10-24 21:38:19 -0600

    built out Division basic operation and validations

--- app/models/concerns/organization/division.rb
+++ app/models/concerns/organization/division.rb
@@ -1,19 +1,19 @@
 module Organization
     class Division < ApplicationRecord
-        # has_many :archer_categories
-        # has_many :gov_bodies, through: :archer_categories
+        has_many :archer_categories
+        has_many :gov_bodies, through: :archer_categories
 
         # all attrs - :name
         
-        # validates :name, 
-        #     presence: { message: "You must enter a name." }, 
-        #     uniqueness: { case_sensitive: false, message: "That name is already taken." }
-        # before_validation :format_name
+        validates :name, 
+            presence: { message: "You must enter a name." }, 
+            uniqueness: { case_sensitive: false, message: "That name is already taken." }
+        before_validation :format_name
 
         # helpers (callbacks & validations)
-        # def format_name
-        #     self.name = self.name.titlecase
-        # end
+        def format_name
+            self.name = self.name.titlecase
+        end
 
         # helpers (data)
         
