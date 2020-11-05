5d362f57bc238ec278ca7c0fb8804b28fa5b25b9
marcon29 <online@markstabler.com>
2020-11-05 09:20:13 -0700

    generated static controller, stubbed main layout, basic layou out design

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  
+
+  root "static#home"
+  # get "home", to: "static#home"
+  get "about", to: "static#about"
+
   resources :rounds, only: [:index, :new, :create]
   resources :score_sessions
   resources :archers, only: [:show, :new, :create, :edit, :update]
