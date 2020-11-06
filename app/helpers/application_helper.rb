aedc67866ada5142f97904918e716b71f80e87f6
marcon29 <online@markstabler.com>
2020-11-06 15:54:34 -0700

    added a couple helpers for home and Archer/show views

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,18 @@
 module ApplicationHelper
+    
+    def active_score_session?
+        current_user.score_sessions.any? && current_user.score_sessions.last.active
+    end
+    
+    def formatted_date(date)
+        date.strftime("%B%e, %Y")
+    end
 
-    # add a method to check for active score sessions for application.html.erb and home.html.erb
+    # adding this also - may want to use when displaying ScoreSession instead of long format above
+    # delete if don't use
+    def formatted_date_short(date)
+        date.strftime("%m/%d/%Y")
+    end
+
+    
 end
