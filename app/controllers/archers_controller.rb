d68d262a17a394c63694d3ede61bfc5abaddb4df
marcon29 <online@markstabler.com>
2020-11-06 12:58:59 -0700

    adjusted archer form design to handle validation errors correctly

--- app/controllers/archers_controller.rb
+++ app/controllers/archers_controller.rb
@@ -8,20 +8,15 @@ class ArchersController < ApplicationController
     @divisions = Organization::Division.all
     @age_classes = Organization::AgeClass.all
   end
-  # <%= f.text_field :gender, class: "form-input-field archer-input", placeholder: (@archer.errors[:password].first || "Select your gender *") %>
-  # <%= f.text_field :default_age_class, class: "form-input-field archer-input", placeholder: (@archer.errors[:password].first || "Select your primary competitive age class") %>
-  # <%= f.text_field :default_division, class: "form-input-field archer-input", placeholder: (@archer.errors[:password].first || "Select your primary shooting style *") %>
-
-  
-
 
   def create
-    @archer = Archer.new
+    @archer = Archer.new(archer_params)
     @genders = Organization::Gender.all
     @divisions = Organization::Division.all
     @age_classes = Organization::AgeClass.all
 
-    check = params[:archer]
+    
+    binding.pry
     render :new
   end
 
@@ -42,6 +37,26 @@ class ArchersController < ApplicationController
     check = params[:archer]
     render :edit
   end
+
+
+  def archer_params
+    params.require(:archer).permit(
+      :username, 
+      :email, 
+      :password, 
+      :first_name, 
+      :last_name, 
+      :birthdate, 
+      :gender_id, 
+      :home_city, 
+      :home_state, 
+      :home_country, 
+      :default_age_class, 
+      :default_division
+    )
+  end
+
+
 
   
 end
