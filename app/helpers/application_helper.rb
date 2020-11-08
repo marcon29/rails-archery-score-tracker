f87714ebc6c914c111e48ed061059cf7dd7e2281
marcon29 <online@markstabler.com>
2020-11-08 12:14:50 -0700

    adjusted model forms to display better with or without notes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,22 +2,24 @@ module ApplicationHelper
     FORM_NOTES = {
         new: {
             comp_section: "You can change these at any time.", 
-            age_class: "If you don't choose, the youngest eligible age class will be automatically assigned (requires you enter your birth date).", 
+            default_age_class: "If you don't choose, the youngest eligible age class will be automatically assigned (requires you enter your birth date).", 
             end_date: "Leave blank if score session is single day.", 
             rank: 'Leave blank unless entering an old score session. Otherwise enter a number above 0, "W" or "L".'
         }, 
         edit: {
             password: "Leave blank if you are not changing your password.", 
             comp_section: "Used to reduce entering info when starting new Score Session.", 
-            age_class: "Only shows Age Classes you're eligible for.", 
+            default_age_class: "Only shows Age Classes you're eligible for.", 
             end_date: "Leave blank if score session is single day.", 
             rank: 'Enter a number above 0, "W" or "L".'
         }
     }
-    
-    def error_check(object, attr)
-        if object.errors[attr].any?
-            tag.p object.errors[attr].first, class: "small-text red-text" 
+
+    def form_note(placement)
+        if form_type == "new" && FORM_NOTES[:new][placement]
+            tag.p FORM_NOTES[:new][placement], class: "small-text" 
+        elsif form_type == "edit" && FORM_NOTES[:edit][placement]
+            tag.p FORM_NOTES[:edit][placement], class: "small-text"
         else
             tag.br
         end
@@ -28,6 +30,21 @@ module ApplicationHelper
             "new"
         elsif params[:action] == "edit" || params[:action] == "update" 
             "edit" 
+        end
+    end
+
+    def error_check(object, attr)
+        if object.errors[attr].any?
+            tag.p object.errors[attr].first, class: "small-text red-text" 
+        end
+    end
+
+    def form_input_class(placement)
+        # "form-input-field model-input"
+        if form_note(placement) == "<br>"
+            "form-input-field model-input"
+        else
+            "form-input-field model-input remove-bottom-margin"
         end
     end
 
@@ -47,16 +64,6 @@ module ApplicationHelper
         params[:action] == "score"
     end
     
-
-    def form_note(placement)
-        if form_type == "new"
-            tag.p FORM_NOTES[:new][placement], class: "small-text" 
-        elsif form_type == "edit"
-            tag.p FORM_NOTES[:edit][placement], class: "small-text" 
-        end
-    end
-
-    # display_rank(rset)
 
     def get_rank(object)
         object.rank.blank? ? "N/A" : object.rank
